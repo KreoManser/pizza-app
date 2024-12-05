47a88f9407bffd3a879db93062e5631261b74cf3
Sergey Babich <sherba.ru@icloud.com>
2024-12-05 22:17:03 +0300

    final

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,16 +1,34 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Headling from '../../components/Headling/Headling';
-import { RootState } from '../../store/store';
+import { AppDispatch, RootState } from '../../store/store';
 import CartItem from '../../components/CartItem/CartItem';
 import { useEffect, useState } from 'react';
 import { IProduct } from '../../interfaces/product.interface';
 import axios from 'axios';
 import { PREFIX } from '../../helpers/API';
 import styles from './Cart.module.css';
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { cartActions } from '../../store/cart.slice';
+
+const DELIVERY_FEE = 250;
 
 export function Cart() {
   const [cartProducts, setCartProducts] = useState<IProduct[]>([]);
   const items = useSelector((s: RootState) => s.cart.items);
+  const jwt = useSelector((s: RootState) => s.user.jwt);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const totalPrice = items
+    .map((i) => {
+      const product = cartProducts.find((p) => p.id === i.id);
+      if (!product) {
+        return 0;
+      }
+      return i.count * product.price;
+    })
+    .reduce((acc, i) => (acc += i), 0);
 
   const getItem = async (id: number) => {
     const { data } = await axios.get<IProduct>(`${PREFIX}/products/${id}`);
@@ -20,6 +38,22 @@ export function Cart() {
   const loadAllItems = async () => {
     const res = await Promise.all(items.map((i) => getItem(i.id)));
     setCartProducts(res);
+  };
+
+  const checkout = async () => {
+    await axios.post(
+      `${PREFIX}/order`,
+      {
+        products: items,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    dispatch(cartActions.clean());
+    navigate('/success');
   };
 
   useEffect(() => {
@@ -36,6 +70,36 @@ export function Cart() {
         }
         return <CartItem key={product.id} count={i.count} {...product} />;
       })}
+      <div className={styles['line']}>
+        <div className={styles['text']}>Итог</div>
+        <div className={styles['price']}>
+          {totalPrice}&nbsp;
+          <span>₽</span>
+        </div>
+      </div>
+      <hr className={styles['hr']} />
+      <div className={styles['line']}>
+        <div className={styles['text']}>Доставка</div>
+        <div className={styles['price']}>
+          {DELIVERY_FEE}&nbsp;
+          <span>₽</span>
+        </div>
+      </div>
+      <hr className={styles['hr']} />
+      <div className={styles['line']}>
+        <div className={styles['text']}>
+          Итог <span className={styles['total-count']}>({items.length})</span>
+        </div>
+        <div className={styles['price']}>
+          {totalPrice + DELIVERY_FEE}&nbsp;
+          <span>₽</span>
+        </div>
+      </div>
+      <div className={styles['checkout']}>
+        <Button appearance="big" onClick={checkout}>
+          ОФОРМИТЬ
+        </Button>
+      </div>
     </>
   );
 }
